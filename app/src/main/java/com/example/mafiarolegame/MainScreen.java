ef19a60e96af5453c77894488449a6fb51a517b8
CloudHare <jan.rimantas@gmail.com>
2019-09-27 14:05:20 +0300

    secnode Commit <3

---
+++ app/src/main/java/com/example/mafiarolegame/MainScreen.java
@@ -0,0 +1,14 @@
+package com.example.mafiarolegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
