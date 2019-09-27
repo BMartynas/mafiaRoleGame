5d320a892ed40d7fd9a290a9f8119672c0c29804
CloudHare <jan.rimantas@gmail.com>
2019-09-27 14:30:19 +0300

    secnode Commit <3

--- app/src/main/java/com/example/mafiarolegame/MainScreen.java
+++ app/src/main/java/com/example/mafiarolegame/MainScreen.java
@@ -3,12 +3,21 @@ package com.example.mafiarolegame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainScreen extends AppCompatActivity {
+
+    private Button createGameButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        createGameButton = (Button) findViewById(R.id.button6);
+        //button.setOnClickListener(new View.OnClickListener());
     }
+
+
 }
