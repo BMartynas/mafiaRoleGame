45efaa86afb86ed39fb226b426928c7ef3563c02
CloudHare <jan.rimantas@gmail.com>
2019-09-27 15:00:02 +0300

    secnode Commit <3

--- app/src/main/java/com/example/mafiarolegame/MainScreen.java
+++ app/src/main/java/com/example/mafiarolegame/MainScreen.java
@@ -2,6 +2,7 @@ package com.example.mafiarolegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,8 +16,19 @@ public class MainScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
-        createGameButton = (Button) findViewById(R.id.button6);
-        //button.setOnClickListener(new View.OnClickListener());
+        createGameButton = (Button) findViewById(R.id.create);
+        createGameButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                testPrint();
+            }
+        });
+    }
+
+    public void testPrint() {
+        //Log.e("magical treasure appears");
+        //Intent intent = new Intent(this, MainActivity.class);
+        //startActivity(intent);
     }
 
 
